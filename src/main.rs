6616150be90d8230033b4968da5da53cdec22a3d
Bruno Santos <bruno.rodrigo@ifood.com.br>
2023-12-16 16:23:43 -0300

    3 first parts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, process};
+
+use rust_book_minigrep::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else( |err| {
+        println!("Error while parsing args {}",err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In the {}", config.file_path);
+    if let Err(e) = rust_book_minigrep::run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
+
