3f13f5c52b1e5f43f345db0928617cf054894eaa
Bruno Santos <bruno.rodrigo@ifood.com.br>
2024-01-07 12:53:30 -0300

    end chapter 12

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ fn main() {
     let args : Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else( |err| {
-        println!("Error while parsing args {}",err);
+        eprintln!("Error while parsing args {}",err);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In the {}", config.file_path);
     if let Err(e) = rust_book_minigrep::run(config){
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
